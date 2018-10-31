3ff1075a08db6371b1bdd14410d0c9f9e39feacc
Baacs <sinsxcrimes@gmail.com>
2018-10-31 18:59:39 +0700

    initial stage

--- Season 4/homework_04/sheepie.py
+++ Season 4/homework_04/sheepie.py
@@ -4,26 +4,21 @@ shpsz = [86, 66, 75, 92, 100, 47, 219]
 print("Hiya, the name's H and these are my pals' sizes: ", shpsz, sep ="\n")
 print()
 
-maxsz = shpsz[0]
+
 n = int(input("For how many months do you want to keep shearing before traveling? (integer) "))
 for x in range(n):
         print("MONTH", x+1)
 
-        
         for i in range(len(shpsz)):
                 shpsz[i] += 50
         print("A month passed. The pals' sizes increased:", shpsz, sep = "\n")  
 
-        for i in range(len(shpsz)):
-                if maxsz < shpsz[i]:
-                        maxsz = shpsz[i]
-        k = shpsz.index(maxsz)
+        k = shpsz.index(max(shpsz))
+        print("The unfortunate one with the size of",shpsz[k] , "will be sheared.")
         shpsz[k] = 8
-        print("The unfortunate one with the size of", maxsz, "will be sheared.")
         print("Here's the flock after shearing: ", shpsz, sep ="\n")
     
         print()
-
         maxsz = 0
 
 print("Alright, you got this, H. Traveling the world is not something to be taken lightly.")
